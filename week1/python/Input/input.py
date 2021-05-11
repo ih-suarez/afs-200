718fd23274ec77556a76657a7ac5136b2159bc85
Ismael Suarez <spawn@Ismaels-MacBook-Pro.local>
2021-05-10 23:31:24 -0500

    Python

--- week1/python/Input/input.py
+++ week1/python/Input/input.py
@@ -1,6 +1,5 @@
 # Name Input and output
-
 city=input('What is your city of residence? ')
 zipcode=input('What is your zipcode? ')
-print('Your city of residence is ' + city + ' and your zipcode: ' + zipcode)
+print('Your city of residence is', city, 'and your zipcode:', zipcode)
 
