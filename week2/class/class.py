93ff660be1a53b2a862800259cdf6c2ea141effe
Ismael Suarez <spawn@Ismaels-MacBook-Pro.local>
2021-05-22 21:36:53 -0500

    Variables, Class, and Numbers

--- week2/class/class.py
+++ week2/class/class.py
@@ -1,4 +1,5 @@
-def user_input():
+# Class 
+class user_input():
     state = input('What is the state you live in? ')
-    print(f"You live in {state.capitalize()}")
+    print(f"You live in {state.title()}")
 user_input()
