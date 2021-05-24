37f76e5f4313c0b7a8476bd6e24a3716563e47bb
Ismael Suarez <spawn@Ismaels-MacBook-Pro.local>
2021-05-24 01:20:57 -0500

    even or odd

---
+++ week2/evenOrOdd/evenOrOdd.py
@@ -0,0 +1,8 @@
+class user_input_eve_or_odd():
+    number = int(input('What number do you have for me? '))
+    check_if_even_or_odd = number % 2
+    if check_if_even_or_odd == 0:
+        print(f"{number} is Even")
+    else:
+        print(f"{number} is Odd")
+user_input_eve_or_odd()
