859b7676988a3b26f721d269a4d3b891f1a1b2af
Ismael Suarez <spawn@Ismaels-MacBook-Pro.local>
2021-06-05 20:03:22 -0500

    Guessing Game

--- week4/guess.py
+++ week4/guess.py
@@ -19,10 +19,16 @@ def game():
             user_guess = input(f'{name.capitalize()} give it another shot, 1-9 ')
 
         if int(user_guess) == number_to_guess:
+            print(f"You can type 'exit' to stop playing.")
             print(f"{name.capitalize()} you guessed it! You got it in {number_of_guesses} guesses. ")
-            user_guess = input(f'Keep playing... 1-9, hit me with another guess. ')
+            user_guess = input(f"Keep playing... 1-9, hit me with another guess ")
             number_of_guesses = 1
 
         if str(user_guess)  == user_exit:
             print(f"Thank you for playing {name.capitalize()}! ")     
+
+        if user_guess == str():
+            print(f'{user_guess} is not valid. Guess again')
+            user_guess = input(f'{name.capitalize()} guess a number, 1-9 ')
+
 game()
