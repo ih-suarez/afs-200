881081c17add18ebfece1332d9c8f6c7e072959b
ih-suarez <ismaelsrz@yahoo.com>
2021-06-30 15:56:12 -0500

    database set up

--- week7/django/src/app/models.py
+++ week7/django/src/app/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
-  added_date = models.DateTimeField()
-  text = models.CharField(max_length=200)
+  titel = models.CharField(max_length=20, default='Title')
+  description = models.CharField(max_length=60, default='Todo')
+  date_posted = models.CharField(max_length=15, default='')
--- week7/django/src/src/settings.py
+++ week7/django/src/src/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Todo',
+        'USER': 'root',
+        'PASSWORD': 'adminroot',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
@@ -108,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'AMERICA/NEW_YORK'
 
 USE_I18N = True
 
